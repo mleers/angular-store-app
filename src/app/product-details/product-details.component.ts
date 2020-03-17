b6bddd76f20c1a382d528debf6950a98c05f809a
mleers <mleers4@gmail.com>
2020-03-17 13:00:23 -0700

    add cart service and buy button

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
+
 
 
 @Component({
@@ -12,8 +14,15 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product;
 
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
 constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
+
   ) { }
   
   ngOnInit() {
